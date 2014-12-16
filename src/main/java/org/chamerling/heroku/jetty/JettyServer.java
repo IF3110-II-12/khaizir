5b4a5a7064891cb39f535b36f304e5d9188730a7
khaidzir <dzir.shhh@gmail.com>
2014-12-16 18:35:51 +0700

    cobaan

--- src/main/java/org/chamerling/heroku/jetty/JettyServer.java
+++ src/main/java/org/chamerling/heroku/jetty/JettyServer.java
@@ -1,39 +1,56 @@
 package org.chamerling.heroku.jetty;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-public class JettyServer {
-	  /**
-     * @param args
-     */
-    public static void main(String[] args) throws Exception{
-        String webappDirLocation = "src/main/webapp/";
-        
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8080 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "9191";
-        }
+import org.eclipse.jetty.servlet.*;
 
-        Server server = new Server(Integer.valueOf(webPort));
-        WebAppContext root = new WebAppContext();
+import java.io.IOException;
 
-        root.setContextPath("/");
-        root.setDescriptor(webappDirLocation+"/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
-        
-        //Parent loader priority is a class loader setting that Jetty accepts.
-        //By default Jetty will behave like most web containers in that it will
-        //allow your application to replace non-server libraries that are part of the
-        //container. Setting parent loader priority to true changes this behavior.
-        //Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
-        root.setParentLoaderPriority(true);
-        
-        server.setHandler(root);
-        
-        server.start();
-        server.join();   
+public class JettyServer extends HttpServlet {
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+@Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    if (req.getRequestURI().endsWith("/db")) {
+      showDatabase(req,resp);
+    } else {
+      showHome(req,resp);
     }
+  }
 
+	private void showHome(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException {
+
+		// Energy is compatible with mass (E=mc2)
+		/*RelativisticModel.select();
+
+		String energy = System.getenv().get("ENERGY");
+
+		Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+		resp.getWriter().print("E=mc^2: " + energy + " = " + m);*/
+		resp.getWriter().print("tes simple blog");
+
+	}
+
+	private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException {
+		resp.getWriter().print("Tes");
+	}
+
+  public static void main(String[] args) throws Exception{
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new JettyServer()),"/*");
+    server.start();
+    server.join();
+  }
 }
